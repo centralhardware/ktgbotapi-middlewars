bf6e127044dd8bae193c3b76fe290424d4b1e655
alex <alex@centralhardware.me>
2025-08-17 12:40:15 +0800

    refactor stdout logging middleware implementation

--- ktgbotapi-stdout-logging-middleware/src/main/kotlin/StdoutLoggingMiddleware.kt
+++ ktgbotapi-stdout-logging-middleware/src/main/kotlin/StdoutLoggingMiddleware.kt
@@ -1,23 +1,19 @@
+package me.centralhardware.telegram
+
 import dev.inmo.kslog.common.KSLog
 import dev.inmo.kslog.common.error
 import dev.inmo.kslog.common.info
 import dev.inmo.micro_utils.common.Warning
-import dev.inmo.tgbotapi.bot.ktor.middlewares.TelegramBotMiddlewareBuilder
 import dev.inmo.tgbotapi.bot.ktor.middlewares.TelegramBotMiddlewaresPipelinesHandler
 import dev.inmo.tgbotapi.extensions.utils.asContentMessage
 import dev.inmo.tgbotapi.extensions.utils.extensions.raw.from
 import dev.inmo.tgbotapi.types.chat.Chat
-import dev.inmo.tgbotapi.types.chat.PrivateChat
 import dev.inmo.tgbotapi.types.chat.User
 import dev.inmo.tgbotapi.types.message.abstracts.ContentMessage
 import dev.inmo.tgbotapi.types.message.content.*
 import dev.inmo.tgbotapi.types.update.*
 import dev.inmo.tgbotapi.types.update.abstracts.UnknownUpdate
 import dev.inmo.tgbotapi.types.update.abstracts.Update
-import kotlin.collections.forEach
-import kotlin.jvm.javaClass
-import kotlin.let
-import kotlin.onFailure
 
 /**
  * A middleware for logging Telegram Bot API updates to stdout.
@@ -30,7 +26,7 @@ import kotlin.onFailure
  * @return A string in the format "userId(firstName lastName)" or empty string if the user is null
  */
 fun User?.format(): String = this?.let {
-    "${PrivateChat.id.chatId.long}(${PrivateChat.firstName} ${PrivateChat.lastName})"
+    "${id.chatId.long}($firstName $lastName)"
 } ?: ""
 
 /**
@@ -39,7 +35,7 @@ fun User?.format(): String = this?.let {
  * @return A string containing the chat ID or empty string if the chat is null
  */
 fun Chat?.format(): String = this?.let {
-    "${Chat.id.chatId.long}"
+    "${id.chatId.long}"
 } ?: ""
 
 /**
@@ -47,7 +43,7 @@ fun Chat?.format(): String = this?.let {
  *
  * @return A string representation of the message content or null if the content type is unknown
  */
-fun ContentMessage<MessageContent>.format(): String? = when (val messageContent = ContentMessage.content) {
+fun ContentMessage<MessageContent>.format(): String? = when (val messageContent = content) {
     is ContactContent -> "${messageContent.contact}"
     is DiceContent -> "${messageContent.dice}"
     is GameContent -> "${messageContent.game}"
@@ -87,95 +83,95 @@ fun Update.format(): String? {
 
     when (this) {
         is EditMessageUpdate -> {
-            from = EditMessageUpdate.data.from.format()
-            text = "Edit: " + EditMessageUpdate.data.asContentMessage()?.format()
+            from = data.from.format()
+            text = "Edit: " + data.asContentMessage()?.format()
         }
         is MessageUpdate -> {
-            from = MessageUpdate.data.from.format()
-            text = "Receive: " + MessageUpdate.data.asContentMessage()?.format()
+            from = data.from.format()
+            text = "Receive: " + data.asContentMessage()?.format()
         }
         is InlineQueryUpdate -> {
-            from = InlineQueryUpdate.data.from.format()
-            text = InlineQueryUpdate.data.toString()
+            from = data.from.format()
+            text = data.toString()
         }
         is CallbackQueryUpdate -> {
-            from = CallbackQueryUpdate.data.from.format()
-            text = CallbackQueryUpdate.data.toString()
+            from = data.from.format()
+            text = data.toString()
         }
         is EditChannelPostUpdate -> {
-            from = EditChannelPostUpdate.data.from.format()
-            text = EditChannelPostUpdate.data.toString()
+            from = data.from.format()
+            text = data.toString()
         }
         is ChannelPostUpdate -> {
-            from = ChannelPostUpdate.data.from.format()
-            text = ChannelPostUpdate.data.toString()
+            from = data.from.format()
+            text = data.toString()
         }
         is ChosenInlineResultUpdate -> {
-            from = ChosenInlineResultUpdate.data.from.format()
-            text = ChosenInlineResultUpdate.data.toString()
+            from = data.from.format()
+            text = data.toString()
         }
         is ShippingQueryUpdate -> {
-            from = ShippingQueryUpdate.data.from.format()
-            text = ShippingQueryUpdate.data.toString()
+            from = data.from.format()
+            text = data.toString()
         }
         is PreCheckoutQueryUpdate -> {
-            from = PreCheckoutQueryUpdate.data.from.format()
-            text = PreCheckoutQueryUpdate.data.toString()
+            from = data.from.format()
+            text = data.toString()
         }
         is PollUpdate -> {
-            text = PollUpdate.data.toString()
+            text = data.toString()
         }
         is PollAnswerUpdate -> {
-            from = PollAnswerUpdate.data.from.format()
-            text = PollAnswerUpdate.data.toString()
+            from = data.from.format()
+            text = data.toString()
         }
         is MyChatMemberUpdatedUpdate -> {
-            from = MyChatMemberUpdatedUpdate.data.user.format()
-            text = MyChatMemberUpdatedUpdate.data.toString()
+            from = data.user.format()
+            text = data.toString()
         }
         is CommonChatMemberUpdatedUpdate -> {
-            from = CommonChatMemberUpdatedUpdate.data.user.format()
-            text = CommonChatMemberUpdatedUpdate.data.toString()
+            from = data.user.format()
+            text = data.toString()
         }
         is ChatJoinRequestUpdate -> {
-            from = ChatJoinRequestUpdate.data.user.format()
-            text = ChatJoinRequestUpdate.data.toString()
+            from = data.user.format()
+            text = data.toString()
         }
         is ChatMessageReactionUpdatedUpdate -> {
-            from = ChatMessageReactionUpdatedUpdate.data.chat.format()
-            text = ChatMessageReactionUpdatedUpdate.data.toString()
+            from = data.chat.format()
+            text = data.toString()
         }
         is ChatMessageReactionsCountUpdatedUpdate -> {
-            from = ChatMessageReactionsCountUpdatedUpdate.data.chat.format()
-            text = ChatMessageReactionsCountUpdatedUpdate.data.toString()
+            from = data.chat.format()
+            text = data.toString()
         }
         is ChatBoostUpdatedUpdate -> {
-            from = ChatBoostUpdatedUpdate.data.chat.format()
-            text = ChatBoostUpdatedUpdate.data.toString()
+            from = data.chat.format()
+            text = data.toString()
         }
         is ChatBoostRemovedUpdate -> {
-            from = ChatBoostRemovedUpdate.data.chat.format()
-            text = ChatBoostRemovedUpdate.data.toString()
+            from = data.chat.format()
+            text = data.toString()
         }
         is BusinessConnectionUpdate -> {
-            from = BusinessConnectionUpdate.data.user.format()
-            text = BusinessConnectionUpdate.data.toString()
+            from = data.user.format()
+            text = data.toString()
         }
         is BusinessMessageUpdate -> {
-            from = BusinessMessageUpdate.data.from.format()
-            text = BusinessMessageUpdate.data.toString()
+            from = data.from.format()
+            text = data.toString()
         }
         is EditBusinessMessageUpdate -> {
-            from = EditBusinessMessageUpdate.data.from.format()
-            text = EditBusinessMessageUpdate.data.toString()
+            from = data.from.format()
+            text = data.toString()
         }
         is DeletedBusinessMessageUpdate -> {
-            from = DeletedBusinessMessageUpdate.data.chat.format()
-            text = DeletedBusinessMessageUpdate.data.toString()
+            from = data.chat.format()
+            text = data.toString()
         }
         is PaidMediaPurchasedUpdate -> {
-            from = PaidMediaPurchasedUpdate.data.from.format()
-            text = PaidMediaPurchasedUpdate.data.toString()
+            from = data.from.format()
+            text = data.toString()
         }
         is UnknownUpdate -> {}
         else -> return null
@@ -199,16 +195,17 @@ fun Update.format(): String? {
  */
 @OptIn(Warning::class)
 fun TelegramBotMiddlewaresPipelinesHandler.Builder.stdoutLogging() {
-    TelegramBotMiddlewaresPipelinesHandler.Builder.addMiddleware {
-        TelegramBotMiddlewareBuilder.doOnRequestReturnResult { result, _, _ ->
-            runCatching {
-                if (result.getOrNull() !is ArrayList<*>) return@doOnRequestReturnResult null
+    addMiddleware {
+        doOnRequestReturnResult { result, _, _ ->
+            kotlin
+                .runCatching {
+                    if (result.getOrNull() !is ArrayList<*>) return@doOnRequestReturnResult null
 
-                @Suppress("UNCHECKED_CAST")
-                (result.getOrNull() as ArrayList<Update>).forEach { update ->
-                    update.format()?.let { message -> KSLog.info(message) }
+                    @Suppress("UNCHECKED_CAST")
+                    (result.getOrNull() as ArrayList<Update>).forEach { update ->
+                        update.format()?.let { message -> KSLog.info(message) }
+                    }
                 }
-            }
                 .onFailure { error -> KSLog.error("Failed to log request", error) }
             null
         }
